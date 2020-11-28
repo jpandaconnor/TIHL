44cbdb1ef613cf128ddf3b32e734a5576409f50e
Connor Brady <connorbrady@codox.eu>
2020-11-28 20:13:30 +0000

    ADDED Reading inputs

--- BETTER_C/main.c
+++ BETTER_C/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("Test code");
+    
+    char str[100];
+    int i;
+
+    printf("Print something here please\n");
+    
+    // Read 2 inputs
+    scanf("%d %s", &i, &str);
+
+    // Print what has been entered
+    printf("\nEntered %d %s\n", i, str);
+
     return 0;
 }
