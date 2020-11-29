84081b62a06ceaebe1ec91c1787cebb2c904cf56
Connor Brady <connorbrady@codox.eu>
2020-11-29 18:46:41 +0000

    assignment operators...

--- BETTER_C/logic.c
+++ BETTER_C/logic.c
@@ -4,18 +4,17 @@ int main(int argc, char *argv[]) {
     7-6; // This is perfectly valid in C...lol
 
     /**
-     * 
      * Simples, +, -, *, /, %, ++, --  (All the same things really)
-     * 
      */
 
     /**
-     * 
-     * 
      *  &&, ||, !  (Logical operators, same things)
-     * 
-     * 
      */
 
+    /**
+     *  =, +=, -=, *= (Assignment ops....woop)
+     */
+
+    
 
 }
