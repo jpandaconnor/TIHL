aed598422294ad9239820b7b4aa1f2fab120eaf9
Connor Brady <connorbrady@codox.eu>
2020-11-29 18:45:30 +0000

    added logic and arithmetic operators (simples stuff again)

--- BETTER_C/logic.c
+++ BETTER_C/logic.c
@@ -2,4 +2,20 @@
 
 int main(int argc, char *argv[]) {
     7-6; // This is perfectly valid in C...lol
+
+    /**
+     * 
+     * Simples, +, -, *, /, %, ++, --  (All the same things really)
+     * 
+     */
+
+    /**
+     * 
+     * 
+     *  &&, ||, !  (Logical operators, same things)
+     * 
+     * 
+     */
+
+
 }
