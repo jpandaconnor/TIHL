4c063833bacf50b8359955932a0489d4daa5d7a1
Connor Brady <connor.brady@broadstone-engage.com>
2020-12-22 18:36:50 +0000

    ADDED Health bar stuff

--- GMOD_LUA/actual_learning_project/lua/autorun/client/cl_learning_addon.lua
+++ GMOD_LUA/actual_learning_project/lua/autorun/client/cl_learning_addon.lua
@@ -7,10 +7,24 @@ hook.Add(ALA_HOOK_HUDPAINT, "ALA_HUDPaint", function()
   -- Size of box
   local boxWidth, boxHeight = width * .25, height * .1
 
-
   surface.SetDrawColor(0, 0, 0, 100)
   surface.DrawRect(
     width / 2 - boxWidth / 2,
     height / 2 - boxHeight / 2,
     boxWidth, boxHeight)
+
+
+
+
+    -- Health Box
+    local hbWidth = width * .2
+    local hbHeight = height * .04
+
+    surface.SetDrawColor(0, 0, 0, 200)
+    surface.DrawRect(
+      width / 2 - width * .49,
+      height / 2 - height * .49,
+      hbWidth,
+      hbHeight
+    )
 end)
