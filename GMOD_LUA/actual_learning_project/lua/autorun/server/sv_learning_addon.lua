2ced8c6589183a543e28d90edd3601949bb9af36
Connor Brady <connorbrady@codox.eu>
2021-01-01 22:32:31 +0000

    ADDED MySQL Database connection stuff

--- GMOD_LUA/actual_learning_project/lua/autorun/server/sv_learning_addon.lua
+++ GMOD_LUA/actual_learning_project/lua/autorun/server/sv_learning_addon.lua
@@ -1,14 +1,28 @@
+require ("mysqloo")
+
 include("autorun/sh_learning_addon.lua")
 
 print("Addon loading: STARTED")
 
-local DATABASE_HOST = "localhost"
+local DATABASE_HOST = "127.0.0.1"
 local DATABASE_PORT = 3306
 local DATABASE_NAME = "gmod"
 local DATABASE_USERNAME = "root"
 local DATABASE_PASSWORD = "teamo14"
 
+logDB = mysqloo.connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME, DATABASE_PORT)
 
+function logDB:onConnectionFailed( err )
+  print( "Connection to database failed!" )
+  print( "Error:", err )
+end
+
+function logDB:onConnected()
+  print( "Database has connected!" )
+end
+
+logDB:connect()
+logDB:wait()
 
 
 hook.Add(ALA_HOOK_PLAYERSAY, "ALA_PlayerSay", function(sender, text, teamChat)
