a5ce72467c34652f2d1f68311e26db7b1ef5afdb
Connor Brady <connorbrady@codox.eu>
2021-01-01 20:58:21 +0000

    GMOD Lua stuff on Linux this time

--- GMOD_LUA/actual_learning_project/lua/autorun/server/sv_learning_addon.lua
+++ GMOD_LUA/actual_learning_project/lua/autorun/server/sv_learning_addon.lua
@@ -2,6 +2,10 @@ include("autorun/sh_learning_addon.lua")
 
 print("Addon loading: STARTED")
 
+require ("mysqloo")
+
+
+
 hook.Add(ALA_HOOK_PLAYERSAY, "ALA_PlayerSay", function(sender, text, teamChat)
   -- Check if player has bypass permissions
   -- if (sender:IsAdmin()) then return end
