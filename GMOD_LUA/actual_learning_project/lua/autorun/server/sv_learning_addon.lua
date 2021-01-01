8fe9a4b5435233ea96b93f9495c9ad8ca2477a3d
Connor Brady <connorbrady@codox.eu>
2021-01-01 23:37:47 +0000

    ADDED (Finally) Database insert

--- GMOD_LUA/actual_learning_project/lua/autorun/server/sv_learning_addon.lua
+++ GMOD_LUA/actual_learning_project/lua/autorun/server/sv_learning_addon.lua
@@ -32,10 +32,24 @@ hook.Add(ALA_HOOK_PLAYERSAY, "ALA_PlayerSay", function(sender, text, teamChat)
   if string.find(text, "fuck") then
     print("This player is a right cheeky cunt")
   end
+
+  addPlayerChatLog(text)
 end)
 
 function addPlayerChatLog(text)
+  local preparedQuery = logDB:prepare("INSERT INTO logs_player_chat (`text`, `created_at`) VALUES(?, ?)")
+
+  preparedQuery:setString(1, text)
+  preparedQuery:setString(2, os.date("!%Y-%m-%d %T"))
+  preparedQuery:start()
+
+  function preparedQuery:onSuccess(data)
+    print("Rows inserted successfully!")
+  end
   
+  function preparedQuery:onError(err)
+    print("An error occured while executing the query: " .. err)
+  end
 end
 
 print("Addon loading: FINISHED")
