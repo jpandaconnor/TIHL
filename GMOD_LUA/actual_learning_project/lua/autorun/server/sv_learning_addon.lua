e19762f46cdc0bcd53e191d377f52588c7badfd7
Connor Brady <connorbrady@codox.eu>
2021-01-03 21:03:29 +0000

    ADDED Dump function for easier debugging
    https://stackoverflow.com/questions/9168058/how-to-dump-a-table-to-console

--- GMOD_LUA/actual_learning_project/lua/autorun/server/sv_learning_addon.lua
+++ GMOD_LUA/actual_learning_project/lua/autorun/server/sv_learning_addon.lua
@@ -28,9 +28,9 @@ logDB:wait()
 
 -- Add players to the database
 hook.Add(AKA_HOOK_PLAYERAUTH, "ALA_" .. AKA_HOOK_PLAYERAUTH, function(ply, steamid, uniqueid)
-  print("AUTH CALLED")
-  print(uniqueid)
-  print(steamid)
+  addPlayer(steamid)
+
+  local player = getPlayer(steamid)
 end)
 
 hook.Add(ALA_HOOK_PLAYERSAY, "ALA_PlayerSay", function(sender, text, teamChat)
@@ -43,6 +43,51 @@ hook.Add(ALA_HOOK_PLAYERSAY, "ALA_PlayerSay", function(sender, text, teamChat)
 
   addPlayerChatLog(text)
 end)
+
+
+function addPlayer(steamid)
+  local preparedQuery = logDB:prepare("INSERT IGNORE INTO players (`steam_id`, `created_at`) VALUES(?, ?)")
+
+  preparedQuery:setString(1, steamid)
+  preparedQuery:setString(2, os.date("!%Y-%m-%d %T"))
+  preparedQuery:start()
+
+  function preparedQuery:onSuccess(data)
+    print("Rows inserted successfully!")
+  end
+  
+  function preparedQuery:onError(err)
+    print("An error occured while executing the query: " .. err)
+  end
+end
+
+function incrementJoinCount(steamid)
+  local preparedQuery = logDB:prepare("INSERT IGNORE INTO players (`steam_id`, `created_at`) VALUES(?, ?)")
+
+end
+
+function getPlayer(steamid)
+  local query = logDB:prepare("SELECT * FROM players WHERE `steam_id` = ? LIMIT 1")
+
+  query:setString(1, steamid)
+
+  print(query)
+  print(steamid)
+
+  function query:onSuccess(data)
+    local playerData = data[1]
+    print(dump(playerData))
+    return playerData
+  end
+
+  function query:onError(err)
+    print(err)
+    print("Error occured whilst getting the query")
+  end
+
+  query:start()
+end
+
 
 function addPlayerChatLog(text)
   local preparedQuery = logDB:prepare("INSERT INTO logs_player_chat (`text`, `created_at`) VALUES(?, ?)")
