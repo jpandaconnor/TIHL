129317a2c4871514e9d89a5897c5a35ddd4477e7
Connor Brady <connorbrady@codox.eu>
2021-03-01 00:22:57 +0000

    ADDED Some bits about Bitwise

---
+++ BETTER_C/bitwise.c
@@ -0,0 +1,11 @@
+// Bitwise stuff
+
+// Not used in common programming language
+// Used to store a lot data
+
+// & - Binary AND - Copies a bit to the result if it exists in both operands
+// | - Binary OR - Copies a bit to the result if it exists in either operand
+// ^ - Binary XOR - Copies the bit if it is set in one operand but not both
+// ~ - Binary Ones - Complement operator is unary and has the effect of flipping bits
+// << - Binary Left Shift Operator - The left operands value is moved left by the number specified by the right operand
+// >> - Binary Right Shift Operator - The left operands value is moved right by the number specified by the right operand
