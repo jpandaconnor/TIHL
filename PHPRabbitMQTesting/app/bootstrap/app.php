f7bf47c32cf2694a15f8fc3df010ac630038f553
Connor Brady <connor.brady@broadstone-engage.com>
2021-05-18 21:36:07 +0100

    ADDED Config AMQP

--- PHPRabbitMQTesting/app/bootstrap/app.php
+++ PHPRabbitMQTesting/app/bootstrap/app.php
@@ -60,6 +60,7 @@ $app->singleton(
 */
 
 $app->configure('app');
+$app->configure('amqp');
 
 /*
 |--------------------------------------------------------------------------
@@ -91,9 +92,10 @@ $app->configure('app');
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\EventServiceProvider::class);
+$app->register(Anik\Amqp\ServiceProviders\AmqpServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
