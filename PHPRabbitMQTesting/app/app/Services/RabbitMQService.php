59fafec8217c5df27a4c52f51fa7e399620dd1e7
Connor Brady <connor.brady@broadstone-engage.com>
2021-05-21 20:43:54 +0100

    ADDED Exception on handling empty topics

--- PHPRabbitMQTesting/app/app/Services/RabbitMQService.php
+++ PHPRabbitMQTesting/app/app/Services/RabbitMQService.php
@@ -4,17 +4,21 @@ namespace App\Services;
 
 class RabbitMQService
 {
-    protected $ampq;
+    protected $amqp;
 
     public function __construct() {
-        $this->ampq = app('amqp');
+        $this->amqp = app('amqp');
     }
 
     // Send = Send and wait for a reply
     public function send(string $service, string $cmd, $data, $callback) {
+        if(empty($service) || empty($cmd)) {
+            throw new \Exception('Service and cmd cannot be empty or null');
+        }
+
         $topic = $this->createMessageTopic($service, $cmd);
 
-        $this->ampq->publish(json_encode($data), $topic, [
+        $this->amqp->publish(json_encode($data), $topic, [
             'exchange' => [
                 'type'    => 'direct',
                 'name'    => 'orka.requestreply',
