9a3ccf90245d8b65e0d2550328aeb8ef1a7981a8
Connor Brady <connor.brady@broadstone-engage.com>
2021-05-21 20:42:21 +0100

    WIP Doing the service class

--- PHPRabbitMQTesting/app/app/Services/RabbitMQService.php
+++ PHPRabbitMQTesting/app/app/Services/RabbitMQService.php
@@ -4,18 +4,26 @@ namespace App\Services;
 
 class RabbitMQService
 {
-    public function __construct() {
+    protected $ampq;
 
+    public function __construct() {
+        $this->ampq = app('amqp');
     }
 
     // Send = Send and wait for a reply
-    public function send(string $topic, $data) {
-        app('amqp')->publish(json_encode(['message' => 'Hello']), 'env.user.sendMessage', [
+    public function send(string $service, string $cmd, $data, $callback) {
+        $topic = $this->createMessageTopic($service, $cmd);
+
+        $this->ampq->publish(json_encode($data), $topic, [
             'exchange' => [
                 'type'    => 'direct',
                 'name'    => 'orka.requestreply',
                 'declare' => true,
             ],
         ]);
+    }
+
+    protected function createMessageTopic(string $service, string $cmd): string {
+        return $service . '.cmd.' . $cmd;
     }
 }
