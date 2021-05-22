ec24ee7ef440269966503b0941f2623ea5f30e2f
Connor Brady <connor.brady@broadstone-engage.com>
2021-05-22 23:16:07 +0100

    FIXED Some mega mega event spam

--- PHPRabbitMQTesting/anp/src/decorators/mq-message.decorator.ts
+++ PHPRabbitMQTesting/anp/src/decorators/mq-message.decorator.ts
@@ -1,12 +1,12 @@
-import { RabbitSubscribe } from "@golevelup/nestjs-rabbitmq";
+import { RabbitRPC, RabbitSubscribe } from "@golevelup/nestjs-rabbitmq";
 
 export function MQMessage (service: string, cmd: string): any {
   const routingKey = service + '.cmd.' + cmd;
   const queue = service + '-' +
     cmd.replace('.', '-').toLowerCase() + '-queue';
 
-  return RabbitSubscribe({
-    exchange: 'orka.requestreply',
+  return RabbitRPC({
+    exchange: 'orka.cmd',
     routingKey: routingKey,
     queue: queue,
   });
