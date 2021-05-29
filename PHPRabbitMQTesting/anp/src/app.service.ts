415c3f3bbf889fd2dd5d18d44d7e13ced5699171
Connor Brady <connor.brady@broadstone-engage.com>
2021-05-29 10:11:51 +0100

    UPDATED Queue names to include service name

--- PHPRabbitMQTesting/anp/src/app.service.ts
+++ PHPRabbitMQTesting/anp/src/app.service.ts
@@ -16,30 +16,11 @@ export class AppService {
     routingKey: 'user.getNotification',
   })*/
 
-  @MQMessage('user', 'sendNotification')
-  public async getNotifications(msg: {}, amqpMsg: ConsumeMessage) {
-    console.log("Get Notifications called");
-    console.log(amqpMsg);
-  }
-
-
-  @MQMessage('user', 'getOneNotification')
-  public async getNotificationsFunction2(msg: {}, amqpMsg: ConsumeMessage) {
-    console.log(msg);
-    return {'message': 'I got a response'};
-  }
-
   @MQEvent('user', 'updated')
   public async handleUserUpdated(msg: {}, amqpMsg: ConsumeMessage) {
     console.log(msg);
     console.log("Event 1 called");
   }
-
-  @MQEvent('user', 'updated')
-  public async handleAnotherConsumer(msg: {}, amqpMsg: ConsumeMessage) {
-    console.log("ANP - Consumer 1 called");
-
-    console.log(msg);
-  }
 }
+
 
