d53054faacfb7e21b744d963a6bb6aec8316b8b7
Connor Brady <connor.brady@broadstone-engage.com>
2021-05-29 11:24:09 +0100

    WIP Event ingestion command

--- PHPRabbitMQTesting/app/app/Console/Commands/ProcessMQEvents.php
+++ PHPRabbitMQTesting/app/app/Console/Commands/ProcessMQEvents.php
@@ -1,6 +1,9 @@
 <?php
 
-class ProcessMQEvents extends \Illuminate\Console\Command
+use App\Services\MQService;
+use Illuminate\Console\Command;
+
+class ProcessMQEvents extends Command
 {
     /**
      * The name and signature of the console command.
@@ -16,5 +19,27 @@ class ProcessMQEvents extends \Illuminate\Console\Command
      */
     protected $description = 'Listeners for events on RabbitMQ';
 
-    
+    protected $mqService;
+
+    private $connection;
+    private $channel;
+
+    public function __construct()
+    {
+        $this->mqService = new MQService();
+
+        $this->connection = $this->mqService->getMQConnection();
+        $this->channel = $this->connection->channel();
+
+        parent::__construct();
+    }
+
+    public function handle() {
+        $this->channel->basic_consume(
+            'user.event.updated',
+            '', false, true, false, false, function() {
+                dump('called');
+        });
+    }
+
 }
