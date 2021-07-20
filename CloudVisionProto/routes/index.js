b826f37eb7eadd332cbb8512e2fac9ed81700568
Connor Brady <connor.brady@broadstone-engage.com>
2021-07-20 14:28:07 +0100

    WIP Project setup

--- CloudVisionProto/routes/index.js
+++ CloudVisionProto/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/cloudvision', function(req, res, next) {
+  return res.send({"Test123": "Test"})
 });
 
 module.exports = router;
+
+
+
+
