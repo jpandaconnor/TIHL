0e7d5017880084127e875c34d0bed9613b65b69c
Connor Brady <connor.brady@broadstone-engage.com>
2021-07-20 14:47:51 +0100

    WIP Getting auth to work + prototyping

--- CloudVisionProto/routes/index.js
+++ CloudVisionProto/routes/index.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient({
+  keyFilename: './creds.json'
+});
+
+
+const images = new Map([
+    // British Gas Bill
+    ['1', './images/british-gas-uk-bill-01.jpeg']
+]);
+
 /* GET home page. */
-router.get('/cloudvision', function(req, res, next) {
+router.get('/cloudvision/:id', async function(req, res, next) {
+  const id = req.params.id;
+
+  const imagePath = images.get(id);
+
+  try {
+    const [result] = await client.labelDetection(imagePath);
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log(id);
+
   return res.send({"Test123": "Test"})
 });
 
