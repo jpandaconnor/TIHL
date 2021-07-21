4721667c9d0e904e7801c2d683b6525471603423
Connor Brady <connorbrady@codox.eu>
2021-07-21 19:09:39 +0100

    Final testing for PHP

--- MakeLoggingBetter/routes/web.php
+++ MakeLoggingBetter/routes/web.php
@@ -13,8 +13,14 @@
 
 $router->get('/', function () use ($router) {
     try {
+        \Illuminate\Support\Facades\Log::info('test123', [
+            'id' => md5('This is a string'),
+            'nick' => md5('This is a string'),
+        ]);
 
-        \Illuminate\Support\Facades\Log::error('test123');
+        \Illuminate\Support\Facades\Log::error('test123', [
+            'id' => md5('This is a string'),
+        ]);
     } catch (Exception $e) {
         dump($e);
     }
